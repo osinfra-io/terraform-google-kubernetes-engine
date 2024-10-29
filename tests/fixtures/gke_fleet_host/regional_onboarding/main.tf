204fe894f1eddc12b478c712462920bd8fd78211
Brett Curtis <brett@osinfra.io>
2024-10-29 19:24:59 -0400

    Remove unused Kubernetes provider and Google client configuration

--- tests/fixtures/gke_fleet_host/regional_onboarding/main.tf
+++ tests/fixtures/gke_fleet_host/regional_onboarding/main.tf
@@ -12,33 +12,6 @@ terraform {
   }
 }
 
-# Kubernetes Provider
-# https://registry.terraform.io/providers/hashicorp/kubernetes/latest
-
-provider "kubernetes" {
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.this.master_auth[0].cluster_ca_certificate
-  )
-
-  host  = data.google_container_cluster.this.endpoint
-  token = data.google_client_config.current.access_token
-}
-
-# Google Client Config Data Source
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
-
-data "google_client_config" "current" {
-}
-
-# Google Container Cluster Data Source
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/container_cluster
-
-data "google_container_cluster" "this" {
-  name     = "mock-cluster"
-  location = "mock-region"
-  project  = var.project
-}
-
 # Remote State Data Source
 # https://www.terraform.io/language/state/remote-state-data
 
